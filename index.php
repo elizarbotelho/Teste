0b955891ab24120a230f27a3963dbaad3d06d3cf
User <User@User-PC>
2016-03-15 22:15:38 -0300

    Comit da index1

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
+<!--<!DOCTYPE html>
 
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
-    <body>
+    <body>-->
         <?php
             echo 'teste git';
         ?>
+   <!--
     </body>
 </html>
+-->
